f1cd8f30ad4f86c38231d901b92a9cf3572da2d7
TsolovikosT <teotsolos@hotmail.gr>
2024-11-09 00:41:31 +0200

    fix proxy for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false,      
-        ws: true,
-      }
-    },
-  }
+export default defineConfig(({ mode }) => {
+   // Get the API base URL from environment variables
+    const env = loadEnv(mode, process.cwd(), '')
+    const API_BASE_URL = JSON.stringify(env.API_BASE_URL);
+
+    return {
+      plugins: [react()],
+      server: {
+        port: 5173,
+        proxy: mode === 'development'
+          ? {
+              '/api': {
+                target: 'http://localhost:5000',  // Local backend server
+                changeOrigin: true,
+                secure: false,
+                ws: true,
+              },
+            }
+          : {
+            '/api': {
+              target: API_BASE_URL, 
+              changeOrigin: true,
+              secure: false,
+              ws: true,
+            },
+          },  
+      },
+    }
 })
